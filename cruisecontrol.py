a2d911645db15d23bb1e7ac7fc3a2d0a8f551c3c
Daniel Suero <danielsuerodelarosa@gmail.com>
2024-12-04 01:26:55 +0100

    Update cruisecontrol.py
    Se actualizan las descripciones para una mejor comprensión del programa

--- cruisecontrol.py
+++ cruisecontrol.py
@@ -1,18 +1,17 @@
-import time  # Importa la librería time
+import time  # Con esta libreria podremos simular la aceleración o desaceleración del vehiculo
 
-velocidad_inicial = 40
-
+velocidad_actual = 0 # Velocidad del vehiculo
 fin = ""
 while fin != "si":
     
-    velocidad_deseada = int(input("Seleccione una velocidad: "))
+    velocidad_deseada = int(input("Seleccione una velocidad: ")) # Aqui el usuario seleccionará la velocidad a la que desea estar
 
-    while velocidad_inicial < velocidad_deseada:
-        velocidad_inicial += 2  # Actualiza velocidad_inicial sumando 2
-        print(velocidad_inicial)
-        time.sleep(0.5)  # Pausa de 0.5 segundos entre cada incremento
+    while velocidad_actual < velocidad_deseada:
+        velocidad_actual += 2  # Actualiza la velocidad actual del vehiculo sumando 2
+        print(velocidad_actual)
+        time.sleep(0.5)  # Pausa de 0.5 segundos entre cada incremento para simular la aceleración del vehiculo
 
-    while velocidad_inicial > velocidad_deseada:
-        velocidad_inicial -= 1  # Actualiza velocidad_inicial sumando 2
-        print(velocidad_inicial)
-        time.sleep(0.5)  # Pausa de 0.5 segundos entre cada incremento
+    while velocidad_actual > velocidad_deseada:
+        velocidad_actual -= 1  # Actualiza la velocidad actual del vehiculo restando 2
+        print(velocidad_actual)
+        time.sleep(0.5)  # Pausa de 0.5 segundos entre cada resta para simular la desaceleración del vehiculo
